12e32c6908cb8d391d0dd93f245a018c8bcc1c52
Arpan Patel <patel.arpa@northeasten.edu>
2025-09-27 22:56:47 -0500

    Added check

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt" // For formatted I/O ops
 	"os"  // For OS interface (accessing CLI args)
+
+	"github.com/arps18/andromex/lexer"
 )
 
-func main(){
+func main() {
 	fmt.Println("Andromex REPL(ctrl + c to exit)")
 	_ = os.Args
+
+	// Test lexer
+	input := "2 + 3 * 4"
+	l := lexer.New(input)
+	for tok := l.NextToken(); tok.Type != "EOF"; tok = l.NextToken() {
+		fmt.Printf("%+v\n", tok)
+	}
 }
